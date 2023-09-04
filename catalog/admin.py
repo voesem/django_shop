94a9310cebcfa67e9f655b4bfa574a659c0fb43a
Vladimir Semikov <voesem@ya.ru>
2023-09-04 22:12:48 +0300

    зарегистрированы модели Product и Category

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from catalog.models import Product, Category
+
+
 # Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'price', 'category')
+    list_filter = ('category',)
+    search_fields = ('title', 'description',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title',)
