e3884b68f934b879b70d16bc3312fa413a979edd
Vladimir Semikov <voesem@ya.ru>
2023-09-04 21:07:49 +0300

    созданы модели Product и Category

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=200, verbose_name='наименование')
+    description = models.CharField(max_length=100, verbose_name='описание')
+    image = models.ImageField(upload_to='products/', verbose_name='изображение (превью)', **NULLABLE)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, verbose_name='категория')
+    price = models.IntegerField(verbose_name='цена за покупку')
+    creation_date = models.DateField(verbose_name='дата создания')
+    last_modified_date = models.DateField(verbose_name='дата последнего изменения')
+
+    def __str__(self):
+        return f'{self.title} {self.description}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+        ordering = ('title',)
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=200, verbose_name='наименование')
+    description = models.CharField(max_length=100, verbose_name='описание')
+
+    def __str__(self):
+        return f'{self.title} {self.description}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+        ordering = ('title',)
