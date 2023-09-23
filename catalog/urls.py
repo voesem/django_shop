96b468030f242e90322e3b62e9bf17f6913081ff
Vladimir Semikov <voesem@ya.ru>
2023-09-23 23:04:57 +0300

    Отредактированы адреса страниц

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
 from . import views
+from catalog.apps import CatalogConfig
+
+app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', views.home),
-    path('contacts/', views.contacts)
+    path('', views.home, name='home'),
+    path('contacts/', views.contacts, name='contacts'),
+    path(r'product/(?P<product_id>\w+)\$', views.product, name='product')
 ]
