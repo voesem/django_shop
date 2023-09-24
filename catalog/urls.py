da53497b709cf7b9c1581bb54ed8646e1ec13631
Vladimir Semikov <voesem@ya.ru>
2023-09-24 16:42:26 +0300

    Контроллеры для страниц со списком товаров и отдельным товаром переведены на CBV

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 
 from . import views
 from catalog.apps import CatalogConfig
+from catalog.views import ProductListView, ProductDetailView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', ProductListView.as_view(), name='home'),
     path('contacts/', views.contacts, name='contacts'),
-    path(r'product/(?P<product_id>\w+)\$', views.product, name='product')
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='view_product')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
 from catalog.models import Product
 
 
-def home(request):
-    products_list = Product.objects.all()
-    context = {
-        'object_list': products_list,
-        'title': 'Каталог'
-    }
-    return render(request, 'catalog/home.html', context)
+class ProductListView(ListView):
+    model = Product
+
+
+class ProductDetailView(DetailView):
+    model = Product
 
 
 def contacts(request):
@@ -24,8 +24,3 @@ def contacts(request):
     }
 
     return render(request, 'catalog/contacts.html', context)
-
-
-def product(request, product_id):
-    product = Product.objects.get(id=product_id)
-    return render(request, 'catalog/product.html', locals())
