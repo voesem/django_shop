a721e2d2bcde777857bf7612612e234a9c297c3c
Vladimir Semikov <voesem@ya.ru>
2023-10-22 13:59:02 +0300

    Настроено кеширование всего контроллера отображения данных относительно одного продукта

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from . import views
 from catalog.apps import CatalogConfig
@@ -10,6 +11,6 @@ urlpatterns = [
     path('', ProductListView.as_view(), name='home'),
     path('contacts/', views.contacts, name='contacts'),
     path('create/', ProductCreateView.as_view(), name='create_product'),
-    path('product/<int:pk>/', ProductDetailView.as_view(), name='view_product'),
+    path('product/<int:pk>/', cache_page(60)(ProductDetailView.as_view()), name='view_product'),
     path('update/<int:pk>/', ProductUpdateView.as_view(), name='update_product')
 ]
